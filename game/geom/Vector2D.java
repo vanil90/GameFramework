b70c09e75a6a8e65a8d2415ce7fbdfd36274daab
Ivan Santos <Ivan Santos@Desktop>
2012-11-04 19:33:40 +0000

    Adicionado geom package e algumas classes

---
+++ game/geom/Vector2D.java
@@ -0,0 +1,5 @@
+package game.geom;
+
+public class Vector2D {
+
+}
---
+++ game/gfx/Animation.java
@@ -0,0 +1,10 @@
+package game.gfx;
+
+public class Animation {
+	private Sprite sprites[];
+	
+
+	Animation(Sprite sprites[]) {
+		this.sprites = sprites;
+	}
+}
